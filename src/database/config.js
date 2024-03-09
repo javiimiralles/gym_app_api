a6658a974c91bae29950a23f1290919d16ad5d4f
jmm291 <jmm291@gcloud.ua.es>
2024-03-09 16:40:22 +0100

    pruebas bd

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.DBCONNECTION);
+        // await mongoose.connect(process.env.DBCONNECTION);
+        await mongoose.connect('mongodb+srv://javiimiralles:uEEkyWz9yJceJF6h@clusterjfitness.bkzpb3w.mongodb.net/ClusterJFitness');
         console.log('Connection established with the database');
     } catch (error) {
         console.log(error);
